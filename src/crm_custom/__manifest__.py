a9ad86b29f329e6c2c8aaf75705ee18fbcc80455
khaimook134411 <khaimook.thanaphorn@21sunpassion.com>
2026-09-10 13:42:06 +0700

    bump version

--- src/crm_custom/__manifest__.py
+++ src/crm_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "CRM Custom Module",
-    "version": "1.0.54",
+    "version": "1.0.55",
     "summary": "CRM Custom Module",
     "author": "tk dev",
     "depends": ["base", "web", "mail"],
